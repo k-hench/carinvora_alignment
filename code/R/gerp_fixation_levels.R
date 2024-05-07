bc14360bf6513eced1b6367fd90140d00fda1a89
k-hench <khench@posteo.net>
2024-05-07 15:46:13 +0200

    init fixed gerp estimation

---
+++ code/R/gerp_fixation_levels.R
@@ -0,0 +1,121 @@
+#!/usr/bin/env Rscript
+# Rscript --vanilla gerp_fixation_levels.R <anc.tree> <clade_tree.pdf> <out.vcf> <in_tree>
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+
+library(tidyverse)
+library(glue)
+library(ggraph)
+library(tidygraph)
+library(patchwork)
+library(ggtext)
+library(ggtree)
+# head -n 1 carnivora_set.txt
+
+tree_in <- args[1]
+pdf_out <- args[2]
+vcf_out <- args[3]
+
+fnt_sel = "Arial"
+
+tree_anc <- read.tree(tree_in)
+all_tips <- sort(tree_anc$tip.label)
+
+tree_anc_data <- (tree_anc |>
+  ggtree::groupClade(.node = c(94,95,102, 103, 104, 119,129,
+                               69,88,70, 84, 71)) |>
+  ggtree(aes(color = group)))$data
+
+group_labels <- tibble(
+  label = str_c("Anc", c("00", "01", "02", "05",
+                         "06", "13", "12","22", "21",
+                         "04", "03","08","07")),
+  gr_label = c("Carnivora", "Feliformia","Caniformia","Canidae",
+               "Arctoidea", "Ursidae", "*unnamed*","Pinnipedia", "Musteloidea",
+               "Viverroidea","Felidae","Pantherinae","Felinae"),
+  gr_idx = c(0,8,1,2,
+             3,7,4,6,5,
+             9,10,11,12)
+)
+
+p <- tree_anc_data |>
+  left_join(group_labels) |>
+  ggtree(aes(color = group)) +
+  geom_node_text(aes(label = str_c(str_remove(label, "Anc"), ":", node), filter = !isTip),
+                 color = "red", family = fnt_sel) +
+  geom_node_text(aes(label =gr_label, filter = !isTip,
+                 color = factor(gr_idx)), vjust = 1.8, family = fnt_sel) +
+  geom_tiplab(family = fnt_sel) +
+  scale_color_manual(values = c("black",
+                                RColorBrewer::brewer.pal(4,"Purples"),
+                                RColorBrewer::brewer.pal(3,"Reds"),
+                                RColorBrewer::brewer.pal(3,"Greens"),
+                                RColorBrewer::brewer.pal(3,"Blues")[2:3]))
+
+ggsave(plot = p,
+       filename = pdf_out,
+       height = 9,
+       width = 8,
+       device = cairo_pdf)
+
+# group_labels |> arrange(gr_idx)
+
+# helper function to collect all species belonging to a clade
+extract_species_for_group <- \(gr_contains){
+  tree_anc_data |>
+    filter(isTip,
+           group %in% gr_contains) |>
+    pluck("label")
+}
+
+# helper function to create genotypes based on assignment to a clade
+create_fixed_genotypes <- \(spec_in){
+  tibble(spec = all_tips,
+         GT = if_else(spec %in% spec_in, "1/1", "0/0")) |>
+    pivot_wider(names_from = spec, values_from = GT)
+}
+
+# compile the genotypes fixed within clades
+group_structure <- tibble(
+  gr_idx = 0:12,
+  contains = list(1:12, c(2,3,4,5,6,7),
+                  2, c(4,5,6,7),
+                  c(5,6), 5, 6, 7,
+                  c(9,10,11,12),
+                  9, c(11,12), 11, 12) ) |>
+  left_join(group_labels) |>
+  mutate(species = map(contains, extract_species_for_group),
+         genotypes = map(species, create_fixed_genotypes)
+         )
+
+# group_structure$species[[which(group_structure$gr_idx == 9)]]
+
+# piecing together the elements of the vcf file
+vcf_head <- glue('##fileformat=VCFv4.2
+##fileDate={Sys.Date()}
+##contig=<ID=dummy,length=12,species="none">
+##INFO=<ID=FI,Number=1,Type=String,Description="Clade in which the SNP is fixed.">
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">')
+vcf_cols <- str_c(c("#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT", all_tips),
+                  collapse = "\t")
+
+# exporting the vcf
+vcf_head |> write_lines(file = vcf_out)
+vcf_cols |> write_lines(file = vcf_out, append = TRUE)
+
+group_structure |>
+  mutate(chr = "dummy",
+         pos = gr_idx,
+         id = ".",
+         ref = "A",
+         alt = "T",
+         qual = ".",
+         filter = "PASS",
+         info = str_c("FI=",gr_label),
+         format = "GT") |>
+  filter(gr_idx > 0) |>
+  select(chr:format,genotypes) |>
+  unnest(genotypes) |>
+  write_tsv(file = vcf_out,
+            col_names = FALSE,
+            append = TRUE)
---
+++ code/workflow/rules/estimate_fixed_gerp_val.smk
@@ -0,0 +1,53 @@
+"""
+snakemake -n --rerun-triggers mtime -R fixed_gerp
+
+snakemake --jobs 50 \
+    --latency-wait 30 \
+    -p \
+    --default-resources mem_mb=51200 threads=1 \
+    --use-singularity \
+    --singularity-args "--bind $CDATA" \
+    --use-conda \
+    --rerun-triggers mtime \
+    --cluster '
+      sbatch_delay \
+        --export=ALL \
+        -n {threads} \
+        -e logs/{name}.{jobid}.err \
+        -o logs/{name}.{jobid}.out \
+        --mem={resources.mem_mb}' \
+        --jn job_c.{name}.{jobid}.sh \
+        -R fixed_gerp
+"""
+localrules: extract_ancestral_tree
+
+rule fixed_gerp:
+    input:
+      gerp = "../results/fixed_gerp/fixed_gerp.vcf"
+
+
+rule extract_ancestral_tree:
+    input:
+      hal = '../results/cactus/{name}.hal'.format(name = P_NAME)
+    output:
+      tree = "../results/fixed_gerp/anc.tree"
+    params:
+      txt = '../results/cactus/{name}.txt'.format(name = P_NAME)
+    shell:
+      """
+      head -n 1 {params.txt} > {out.tree}
+      """
+
+rule compile_fixed_gerp_vcf:
+    input:
+      tree = "../results/fixed_gerp/anc.tree"
+    output:
+      vcf = "../results/fixed_gerp/fixed_gerp.vcf",
+      pdf = "../results/img/clades_tree.pdf"
+    log: "logs/fixed_gerp_vcf.log"
+    container: c_conda
+    conda: "r_tidy"
+    shell:
+      """
+      Rscript --vanilla R/gerp_fixation_levels.R {input.tree} {output.pdf} {output.vcf} &>> {log}
+      """
